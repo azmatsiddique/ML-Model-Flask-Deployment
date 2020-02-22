695c0638934103446be52ad4a18ce78b988a84b7
azmatsiddique <azmat.siddique.98@gmail.com>
2020-02-22 10:09:35 +0530

    Add existing file

--- request.py
+++ request.py
@@ -1,6 +1,18 @@
 import requests
-
 url = 'http://localhost:5000/predict_api'
-r = requests.post(url,json={'Mg':20, 'Na':90, 'Mg':6,'Al':55.9,'K':0.3,'Ca':23.0,'B':0,'Fe':0})
+r = requests.post(url, json={
+    'Mg': 1.52101,
+    'Na': 13.64,
+    'Mg': 4.49,
+    'Al': 1.1,
+    'Si': 71.78,
+    'K': 0.06,
+    'Ca': 8.75,
+    'B': 0,
+    'Fe': 0,
+    })
 
-print(r.json())
+return requests.get(r).json()
+
+
+			
